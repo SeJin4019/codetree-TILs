a3264eaab2b176c82b902f2d4269bab115edf275
김세진 <121854655+SeJin4019@users.noreply.github.com>
2024-08-22 22:53:12 +0900

    Title: 일 수 구하기, Time: 108ms, Memory: 10MB, Status: Passed - CodeTree

--- 240822/일 수 구하기/number-of-days-in-month.java
+++ 240822/일 수 구하기/number-of-days-in-month.java
@@ -3,25 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int month = sc.nextInt();
-        if(month == 1 || month == 3 ||
-           month == 5 || month == 7 || 
-           month == 8 || month == 10|| month == 12){
-            System.out.println(31);
-           }else if(month == 4 || month == 6 ||
-           month == 9 || month == 11){
-            System.out.println(30);
-           }else if(month == 2){
-              if(month % 4 == 0){
-                if(month % 100 == 0 && month % 400 != 0){
-                    System.out.println(29);    
-                }else{
-                    System.out.println(28);    
-                }
-            }
-           }
 
-
+        // 변수 선언
+        int n;
         
+        // 입력
+        n = sc.nextInt();
+
+        // 출력
+        if(n == 2)
+            System.out.println("28");
+        else if(n <= 7) {
+            if(n % 2 == 1)
+                System.out.println("31");
+            else
+                System.out.println("30");
+        }
+        else {
+            if(n % 2 == 0)
+                System.out.println("31");
+            else
+                System.out.println("30");
+        }
     }
 }
