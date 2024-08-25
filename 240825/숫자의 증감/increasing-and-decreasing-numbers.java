e273244cbc137d1eb76999fe6990fb1dd12d09f8
김세진 <121854655+SeJin4019@users.noreply.github.com>
2024-08-25 15:12:39 +0900

    Title: 숫자의 증감, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240825/숫자의 증감/increasing-and-decreasing-numbers.java
+++ 240825/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -3,17 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char c = sc.next().charAt(0);
-        int n = sc.nextInt();
         
-        if(c == 'A'){
-            for(int i = 1; i <= n; i++){
+        // 변수 선언
+        char c;
+        int n;
+
+        // 입력
+        c = sc.next().charAt(0);
+
+        n = sc.nextInt();
+
+        // 출력
+        if(c == 'A') {
+            for(int i = 1; i <= n; i++)
                 System.out.print(i + " ");
-            }
-        }else {
-            for(int i = n; n >= 1; i--){
+        }
+        else {
+            for(int i = n; i >= 1; i--)
                 System.out.print(i + " ");
-            }
         }
     }
 }
