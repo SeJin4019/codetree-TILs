f968cea8a64edaca5eb036591937d05a8ed4db03
김세진 <121854655+SeJin4019@users.noreply.github.com>
2024-08-28 23:27:01 +0900

    Title: 1까지 나누기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240828/1까지 나누기/divide-by-1.java
+++ 240828/1까지 나누기/divide-by-1.java
@@ -3,19 +3,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int i = 1;
-        int count = 0;
-
-        while (true){
-            n = n / i;
-            if(n < 1){
-                count++;
-                System.out.print(count);
-                break;
-            }
+        int i=1;
+        while(n>1){
+            n /= i ;
             i++;
-            count++;
         }
+        System.out.print(i-1);
 
     }
 }
