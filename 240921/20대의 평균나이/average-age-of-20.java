6acb9f61f8b0e92b369cb973739b63339524885b
김세진 <121854655+SeJin4019@users.noreply.github.com>
2024-09-21 14:10:30 +0900

    Title: 20대의 평균나이, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240921/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double sum = 0;
+        int count = 0;
+
+        while(true) {
+            int age = sc.nextInt();
+            if(age>29 || age<20) {
+                System.out.printf("%.2f", sum/count);
+                break;
+            }
+            sum+=age;
+            count++;
+        }
+    }
+}
